d1f29c6b5c5c394cb4d0a87ef7654cc216e1ad7d
EddieCornelious <eulerissmart@gmail.com>
2023-03-28 12:01:16 -0400

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const http = require('http');
+
+const app = express();
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/',
+});
+
+app.use('/peerjs', peerServer);
+
+server.listen(9000);
+
+/**
+ * const peer = new Peer("someid", {
+		host: "localhost",
+		port: 9000,
+		path: "/myapp",
+	});
+ */
